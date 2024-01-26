fac9f2ac79ce9793e44c99b54a90bcdc10140718
JuanDaniel <judaniel.cardona@gmail.com>
2024-01-26 08:06:54 -0500

    practica pagina web

--- HelloWorld.py
+++
@@ -1,5 +0,0 @@
-print("Hello world with changes")
-
-print("Change #1")
-print("Change #2")
-print("Change #3")
---
+++ main.js
@@ -0,0 +1,3 @@
+const usuario = document.getElementById("NOMBRE");
+
+usuario.innerHTML = "NOMBRE USUARIO"
